4c66c548dbaad078f375d83a5b5a7bb02b2ec4d2
rayhan108 <raihanshorker56@gmail.com>
2026-02-24 10:25:35 +0600

    fix meta data server component conflict issue

---
+++ src/app/aboutUs/layout.jsx
@@ -0,0 +1,10 @@
+
+
+export const metadata = {
+  title: "About Cathy’s Jewelry | Trusted Chicago Jeweler",
+  description: "Learn about Cathy’s Jewelry, a locally trusted Chicago jewelry store specializing in fine jewelry, custom designs, expert repairs, and gold buying with personalized service.",
+};
+
+export default function AboutLayout({ children }) {
+  return <>{children}</>;
+}
