58d3f6a319e4cba519a9292a5929099388c4f42f
rayhan108 <raihanshorker56@gmail.com>
2026-03-29 14:26:17 +0600

    readd domain

--- src/components/shared/mainUrl.js
+++ src/components/shared/mainUrl.js
@@ -1,7 +1,7 @@
 const mainUrl = async (url) => {
   //  const response = await fetch(`https://api.cathysjewelry.net${url}`);
   //  const response = await fetch(`http://10.10.20.26:3000${url}`);
-   const response = await fetch(`http://13.60.228.122:3000${url}`);
+   const response = await fetch(`https://api.cathysjewelry.net${url}`);
    
    if (!response.ok) {
      throw new Error(`HTTP error! status: ${response?.status}`);
